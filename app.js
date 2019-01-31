f6c3a1074802c05291fecb392c00ab38c21201b0
Muhammad Talha Qadir <mqadir@ucsd.edu>
2019-01-31 10:24:54 -0800

    Lab 4 completion

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -9,6 +8,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
 var hello = require('./routes/hello');
 // Example route
 // var user = require('./routes/user');
@@ -37,6 +37,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+//app.get('/project', project.viewProject);
+app.get('/project/:name', project.viewProject);
 app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
